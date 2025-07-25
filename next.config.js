4f2800af812a953ca492f0fea172d4c99eb1c299
Arseven Ali <arseali@mac.intern>
2025-07-25 15:17:53 +0200

    Sauberes Deployment - alle Dateien aktualisiert

---
+++ next.config.js
@@ -0,0 +1,51 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    esmExternals: false
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+        stream: false,
+        util: false,
+        buffer: false,
+        process: false,
+        os: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        constants: false,
+        querystring: false,
+        punycode: false,
+        domain: false,
+        dns: false,
+        net: false,
+        tls: false,
+        child_process: false,
+        cluster: false,
+        module: false,
+        vm: false,
+        tty: false,
+        readline: false,
+        repl: false,
+        string_decoder: false,
+        timers: false,
+        events: false,
+        _stream_duplex: false,
+        _stream_passthrough: false,
+        _stream_readable: false,
+        _stream_transform: false,
+        _stream_writable: false,
+      }
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig 
